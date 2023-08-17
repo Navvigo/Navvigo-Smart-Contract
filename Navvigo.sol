3f55a857521ffd05c917a83537461030eef6119f
yemre82 <51782698+yemre82@users.noreply.github.com>
2023-08-17 18:00:00 +0300

    Solidity Contract Published

---
+++ Navvigo.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.3/security/Pausable.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
+
+/// @custom:security-contact hello@navvigo.com
+contract NAVVIGO is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("NAVVIGO", "NVVG") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
